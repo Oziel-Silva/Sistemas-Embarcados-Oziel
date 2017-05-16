76857f4e4a567571f7d9d99cdd46f0ce017c9450
Oziel-Silva <oziel.service@live.com>
2017-05-16 13:36:10 -0300

    update

--- Ponto_de_Controle_02/cliente.c
+++ Ponto_de_Controle_02/cliente.c
@@ -11,16 +11,18 @@
 	void sensores(void);
 	void menu(void);
 	void quarto(char *parametro);
+	void sala(char *parametro);
 	void opcao_1(void);
 	int conect(void);
 	void menu_principal(void);
 	void temperatura(char* parametro);
 
-	char IP_Servidor[] = "192.168.25.25";
+	char IP_Servidor[] = "192.168.43.196";
 	int socket_id;
 	struct sockaddr_in servidorAddr;
 	int length;
 	unsigned short servidorPorta = 9000;
+	char* text;
 	
 	
 
@@ -29,51 +31,6 @@ int main (int argc, char* const argv[])
 	menu_principal();
 	return 0;
 }
-
-// void chama_opcao(void)
-// { char* text;
-// 	//fprintf(stderr, "Abrindo o socket para o cliente... ");
-// 	socket_id = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-// 	if(socket_id < 0)
-// 	{
-// 		fprintf(stderr, "Erro na criacao do socket!\n");
-// 		exit(0);
-// 	}
-// 	//fprintf(stderr, "Feito!\n");
-
-// 	//fprintf(stderr, "Conectando o socket ao IP %s pela porta %d... ", IP_Servidor, servidorPorta);
-// 	memset(&servidorAddr, 0, sizeof(servidorAddr)); // Zerando a estrutura de dados
-// 	servidorAddr.sin_family = AF_INET;
-// 	servidorAddr.sin_addr.s_addr = inet_addr(IP_Servidor);
-// 	servidorAddr.sin_port = htons(servidorPorta);
-// 	if(connect(socket_id, (struct sockaddr *) &servidorAddr,sizeof(servidorAddr)) < 0)
-// 	{
-// 		fprintf(stderr, "Erro na conexao!\n");
-// 		exit(0);
-// 	}
-// 	//fprintf(stderr, "Feito!\n");
-
-// 	fprintf(stderr, "Por favor aguarde...\n ");
-// 	length = strlen(a) + 1;
-// 	write(socket_id, &length, sizeof(length));
-// 	write(socket_id, a, length);
-// 	//fprintf(stderr, "Feito!\n");
-// 	//sleep(1);
-
-// 	text = (char*) malloc (length);
-// 	read(socket_id, &length, sizeof (length));
-// 	read(socket_id, text, length);
-// 	fprintf(stderr,"\n\n   Temperatura do ambiente é de: %s °C\n\n", text);
-// 	free (text);
-// 	sleep(3);
-
-
-// 	fprintf(stderr, "Fechando o socket local... ");
-// 	close(socket_id);
-// 	fprintf(stderr, "Feito!\n");
-// 	system("clear");
-// }
-
 
 void print_exemplo()
 {
@@ -95,52 +52,6 @@ void print_exemplo()
 		printf("   Exemplo: 127.0.0.1 8000 \"Ola socket\"\n");
 		exit(1);
 }
-
-
-
-void menu(void)
-{
-
-	
-	//close(socket_id);
-	
-//  do
-//     {
-//         printf("\n\t Menu de Opções\n\n");
-//         printf("1. Sensores\n");
-//         printf("2. Tomadas\n");
-//         printf("3. Câmera\n");
-//         printf("0. Sair\n");
-
-
-//         scanf("%c", &parametro);
-//         system("clear");
-
-//         switch(parametro)
-//         {
-//             case '1':
-//                 //sensores();
-//                 break;
-
-//             case '2':
-//                 printf("caso 2/\n");
-//                 break;
-
-//             case '3':
-//                 printf("caso 3\n");
-//                 break;
-
-//             case '0':
-//                 exit(0);
-//                 break;
-
-//             default:
-//                 printf("Digite uma opcao valida\n");
-//         }
-//     } while(parametro);
-// 
-}
-
 
 void menu_principal(void)
 { 
@@ -173,6 +84,8 @@ void menu_principal(void)
                 break;
 
             case '2' :
+           		param_envio = "2";
+            	sala(param_envio);
                 
                 break;
 
@@ -193,13 +106,53 @@ void menu_principal(void)
 void quarto(char *parametro)
 {
 	
-	int socket = conect();
-	char* param_envio;
-	char escolha;
+	 
+	 char* param_envio;
+	 char escolha;
 
-	length = strlen(parametro);
-	write(socket_id, &length, sizeof(length));
-	write(socket_id, parametro, length);
+	sleep(1);
+	
+	
+  do
+    {
+        printf("\n\tEscolha uma funcionalidade\n\n");
+        printf("1. Tomadas\n");
+        printf("2. Temperatura do recinto\n");
+        printf("0. Voltar ao menu anterior\n");
+
+        scanf(" %c", &escolha);
+
+        system("clear");
+
+        switch(escolha)
+        {
+            case '1':
+            	//param_envio = "1";
+            	//tomadas();
+           		
+            	break;
+            case '2':
+            	param_envio = "1";
+            	temperatura(param_envio);
+            	break; 
+            case '0':
+            	//param_envio = "1";
+            	menu_principal();             
+
+            default:
+                printf("Digite uma opcao valida\n");
+        }
+    } while(param_envio);
+}
+
+void sala(char *parametro)
+{
+	
+	 
+	 char* param_envio;
+	 char escolha;
+
+	sleep(1);
 	
 	
   do
@@ -223,7 +176,10 @@ void quarto(char *parametro)
             case '2':
             	param_envio = "2";
             	temperatura(param_envio);
-            	break;              
+            	break; 
+            case '0':
+            	//param_envio = "1";
+            	menu_principal();             
 
             default:
                 printf("Digite uma opcao valida\n");
@@ -255,6 +211,29 @@ int conect(void)
 }
 
 void temperatura(char* parametro)
-{
+{	char* text;
+	int length_rx;
+	int temp ;
+	int rx_temp;
 	int socket = conect();
+
+
+    length = strlen(parametro);
+	write(socket, &length, sizeof(length));
+	write(socket, parametro, length);
+
+	read(socket, &length_rx, sizeof (length_rx));
+	text = (char*) malloc (length_rx);
+ 	read(socket, text, length_rx);
+
+ 	rx_temp = atoi(text);
+
+ 	temp = (100 * rx_temp)/1024;
+
+	fprintf(stderr,"\n\n   Temperatura do ambiente é de: %d °C\n\n", temp);
+	free (text);
+	sleep(2);
+	system("clear");
+	menu_principal();
+
 }
